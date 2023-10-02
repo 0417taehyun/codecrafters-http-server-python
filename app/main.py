2cb6e7afd713e3fc60fc097737b50d9065e27db5
taehyun <0417taehyun@gmail.com>
2023-10-02 12:50:33 +0900

    feat: Respond with 404 by parsing received data

--- app/main.py
+++ app/main.py
@@ -1,31 +1,43 @@
+import dataclasses
+import enum
 import socket
 
 
-def main():
-    server: socket.socket = socket.create_server(address=("localhost", 4221), reuse_port=True)
 
-    
-    # Accept a connection. The return value is a pair (conn, address)
-    # conn is a new socket object usable to send and receive data on the connection. It is non-inheritable.
-    # address is the address bound to the socket on the other end of the connection
+class HTTPMethod(str, enum.Enum):
+    GET: str = "GET"
+    POST: str = "POST"
+
+
+@dataclasses.dataclass
+class Application:
+    HOST: str = "localhost"
+    PORT: int = 4221
+    BUFFER_SIZE: int = 1024
+
+
+def main():
+    server: socket.socket = socket.create_server(address=(Application.HOST, Application.PORT), reuse_port=True)
     connection, address = server.accept()
 
+    with connection:
+        chunk: bytes = connection.recv(Application.BUFFER_SIZE)
 
-    # Receive data from the socket. The return value is a bytes object
-    # The maximum amount of data to be received at once is specified by bufsize.
-    data: bytes = connection.recv(65536)
+        received_information: str = chunk.decode(encoding="utf-8").split("\r\n")
+        start_line: str = received_information[0]
+        method, path, protocol = start_line.split(" ")
+        
+        if method != HTTPMethod.GET.value:
+            print(f"Method not allowed: {received_information}")
+            connection.send(b"HTTP/1.1 405 Method Now Allowed\r\n\r\n")
+            return
+        
+        if path != "/":
+            print(f"Not Found: {received_information}")
+            connection.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+            return
 
-    # The return bytes object looks like b"GET / HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: Go-http-client/1.1\r\nAccept-Encoding: gzip\r\n\r\n"
-    # \r is a CR(Carriage Return) which means that it moves the cursor to the beginning of the line without advancing to the next line.
-    # \n is a LF(Line Feed) which means that it moves the cursor down to the next line without returning to the beginning of the line.
-    # Thus, CRLF, \r\n means that it moves the cursor down to the next line and then to the beginning of the line.
-    print(data)
-
-    # HTTP Response has three parts, Status Line, Headers, and Body.
-    # Two CRLF, \r\n\r\n, means that the first CRLF signifies the end of the Status Line, and the second CRLF signifies the end of the Headers which is empty in this case.
-    connection.send(b"HTTP/1.1 200 OK\r\n\r\n")
-
-    connection.close()
+        connection.send(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
